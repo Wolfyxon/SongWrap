66331d2068c611335026772469907d4e56f431e6
Wolfyxon <wolfyxon@gmail.com>
2025-12-10 20:04:08 +0100

    Implemented FileUpload logic

--- src/lib/comp/FileUpload.svelte
+++ src/lib/comp/FileUpload.svelte
@@ -1,11 +1,62 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
+
     export let label: string | null = null;
+
+    export let onDropped: ((files: FileList) => any) = (files) => {
+        console.warn("Files were dropped but 'onDropped' callback is not set");
+        console.log(files);
+    };
+    
+    let fileHover = false;
+
+    // TODO: Error display
+
+    function filesDropped(files: FileList | null | undefined) {
+        if(!files || files.length == 0)
+            return;
+
+        onDropped(files);
+    }
+
+    function inputChange(e: Event) {
+        const target = e.target as HTMLInputElement;
+        filesDropped(target.files);
+    }
+
+    onMount(() => {
+        window.addEventListener("drop", (e) => {
+            const target = e.target as HTMLElement;
+            fileHover = false;
+
+            if(target.tagName == "INPUT")
+                return;
+            
+            e.preventDefault();
+            filesDropped(e.dataTransfer?.files);
+        });
+
+        window.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            fileHover = true;
+        });
+
+        window.addEventListener("dragleave", (e) => {
+            fileHover = false;
+        });
+    });
+
 </script>
 
 <style>
     .file-upload {
         padding: 20px;
         border: var(--panel-outline) 2px solid;
+        transition: 0.25s;
+    }
+
+    .file-upload.file-hover {
+        background-color: var(--panel-outline);
     }
 
     .file-upload h1 {
@@ -13,10 +64,10 @@
     }
 </style>
 
-<div class="file-upload panel">
+<div class="file-upload panel" class:file-hover={fileHover}>
     {#if label}
         <h1>{label}</h1>
     {/if}
 
-    Drag and drop a file or use <input type="file" />
+    Drag and drop a file or use <input type="file" onchange={inputChange} />
 </div>
