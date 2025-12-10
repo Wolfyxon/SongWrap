341d0a1bbcb931909dfd7ed67a4f338d0dfdd671
Wolfyxon <wolfyxon@gmail.com>
2025-12-10 13:46:45 +0100

    More style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 
-<h1>
-    SongWrap
-</h1>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem temporibus, magni quisquam ad vero inventore facere similique officiis quo veniam hic a illum dolorem repellendus quos aspernatur nemo distinctio!
-</p>
+<style>
+    #banner {
+        margin: auto;
+        max-width: 1000px;
+    }
+
+    #banner h1 {
+        text-align: center;
+        font-size: 400%;
+    }
+</style>
+
+<main>
+    <div id="banner">
+        <h1>SongWrap</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quidem temporibus, magni quisquam ad vero inventore facere similique officiis quo veniam hic a illum dolorem repellendus quos aspernatur nemo distinctio!
+        </p>
+
+    </div>
+</main>
