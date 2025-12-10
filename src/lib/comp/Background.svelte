dd3354eba63349996d35903f8a941b80dc1f8338
Wolfyxon <wolfyxon@gmail.com>
2025-12-10 13:14:15 +0100

    Background animation and style adjust

--- src/lib/comp/Background.svelte
+++ src/lib/comp/Background.svelte
@@ -1,26 +1,47 @@
 <script lang="ts">
-    import { randf } from "$lib/util/math";
+    import { randf, randi } from "$lib/util/math";
 
     type Ball = {
         color: string,
+        animTime: number,
+        angleOffset: number,
         x: number,
         y: number
     }
 
     const balls: Ball[] = [];
+    const colors = ["A", "B", "C"];
 
     for(let i = 0; i < 10; i++) {
+        const colorLetter = colors[randi(0, colors.length - 1)];
+
         balls[i] = {
-            color: "var(--colorA)",
-            x: randf(0, 100),
-            y: randf(0, 100)
+            color: `var(--color${colorLetter})`,
+            animTime: randi(20, 60),
+            angleOffset: randi(-360, 360),
+            x: randf(-50, 100),
+            y: randf(-50, 100)
         }
     }
 </script>
 
 <style>
+    @keyframes orbit {
+        0% {
+            transform: rotate(0deg) rotate(var(--angle-offset)) translateX(0%);
+        }
+        50% {
+            transform: rotate(360deg) rotate(var(--angle-offset)) translateX(100%);
+        }
+        100% {
+            transform: rotate(0deg) rotate(var(--angle-offset)) translateX(0%);
+        }
+    }
+
     #background {
         position: absolute;
+        overflow: hidden;
+        opacity: 0.2;
         top: 0;
         bottom: 0;
         width: 100%;
@@ -33,8 +54,9 @@
         border-radius: 100%;
         filter: blur(100px);
         background-color: var(--color);
-        width: 100px;
-        height: 100px;
+        width: 35%;
+        aspect-ratio: 1;
+        animation: orbit var(--anim-time) infinite linear;
     }
 </style>
 
@@ -44,6 +66,8 @@
             class="ball" 
             style={`
                 --color: ${ball.color};
+                --anim-time: ${ball.animTime}s;
+                --angle-offset: ${ball.angleOffset}deg;
                 left: ${ball.x}%;
                 top: ${ball.y}%; 
             `}
