1cea75aa70e295e051ff13696c70d3ee029f91df
Wolfyxon <wolfyxon@gmail.com>
2025-12-10 20:29:02 +0100

    Pages are stored in a container

---
+++ src/lib/comp/Page.svelte
@@ -0,0 +1,13 @@
+<!-- TODO: Page title and description -->
+
+<script lang="ts">
+    import Footer from "./Footer.svelte";
+
+    let { children } = $props();
+</script>
+
+<main>
+    {@render children()}
+
+    <Footer />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import FileUpload from "$lib/comp/FileUpload.svelte";
     import Footer from "$lib/comp/Footer.svelte";
+    import Page from "$lib/comp/Page.svelte";
 
 </script>
 
@@ -14,7 +15,7 @@
     }
 </style>
 
-<main>
+<Page>
     <div id="banner">
         <h1>SongWrap</h1>
     </div>
@@ -27,6 +28,4 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ipsum dolorum ipsa enim magni dolore? Iusto quaerat nesciunt dolores ullam obcaecati magnam veniam doloremque odit, cum nihil omnis, illo excepturi.
         </p>
     </div>
-
-    <Footer />
-</main>
+</Page>
