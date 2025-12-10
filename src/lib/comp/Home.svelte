a5fbd1377523830386bfead6695c197702452033
Wolfyxon <wolfyxon@gmail.com>
2025-12-10 20:49:49 +0100

    Moved home to a separate container

---
+++ src/lib/comp/Home.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import FileUpload from "$lib/comp/FileUpload.svelte";
+</script>
+
+<style>
+    #banner {
+        text-align: center;
+    }
+
+    #banner h1 {
+        font-size: 400%;
+    }
+</style>
+
+<div id="banner">
+    <h1>SongWrap</h1>
+</div>
+
+<FileUpload label="Upload a stat file" />
+
+<div>
+    <h2>How to get your stats file?</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ipsum dolorum ipsa enim magni dolore? Iusto quaerat nesciunt dolores ullam obcaecati magnam veniam doloremque odit, cum nihil omnis, illo excepturi.
+    </p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,8 @@
 <script>
-    import FileUpload from "$lib/comp/FileUpload.svelte";
-    import Footer from "$lib/comp/Footer.svelte";
+    import Home from "$lib/comp/Home.svelte";
     import Page from "$lib/comp/Page.svelte";
-
 </script>
 
-<style>
-    #banner {
-        text-align: center;
-    }
-
-    #banner h1 {
-        font-size: 400%;
-    }
-</style>
-
 <Page>
-    <div id="banner">
-        <h1>SongWrap</h1>
-    </div>
-
-    <FileUpload label="Upload a stat file" />
-
-    <div>
-        <h2>How to get your stats file?</h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ipsum dolorum ipsa enim magni dolore? Iusto quaerat nesciunt dolores ullam obcaecati magnam veniam doloremque odit, cum nihil omnis, illo excepturi.
-        </p>
-    </div>
+    <Home />
 </Page>
