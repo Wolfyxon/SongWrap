6a88e09c5596a8293f9d920807377cc279d37f60
Wolfyxon <wolfyxon@gmail.com>
2025-12-11 16:50:48 +0100

    Fixed animations not working on gh pages (probably)

--- src/lib/styles/anim.svelte
+++ src/lib/styles/anim.svelte
@@ -1,46 +1,45 @@
 <style>
-    :global {
-        @keyframes flash {
-            0% {
-                opacity: 1;
-            }
-            50% {
-                opacity: 0.5;
-            }
-            100% {
-                opacity: 1;
-            }
+    @keyframes -global-flash {
+        0% {
+            opacity: 1;
         }
-
-        @keyframes fade-in {
-            0% {
-                opacity: 0;
-            }
-            100% {
-                opacity: 1;
-            }
+        50% {
+            opacity: 0.5;
         }
-
-        @keyframes slide-in-left {
-            0% {
-                opacity: 0;
-                transform: translateX(-35%);
-            }
-            100% {
-                opacity: 1;
-                transform:  translateX(0%);
-            }
-        }
-
-        @keyframes slide-in-bottom {
-            0% {
-                opacity: 0;
-                transform: translateY(100%);
-            }
-            100% {
-                opacity: 1;
-                transform:  translateY(0%);
-            }
+        100% {
+            opacity: 1;
         }
     }
+
+    @keyframes -global-fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    @keyframes -global-slide-in-left {
+        0% {
+            opacity: 0;
+            transform: translateX(-35%);
+        }
+        100% {
+            opacity: 1;
+            transform:  translateX(0%);
+        }
+    }
+
+    @keyframes -global-slide-in-bottom {
+        0% {
+            opacity: 0;
+            transform: translateY(100%);
+        }
+        100% {
+            opacity: 1;
+            transform:  translateY(0%);
+        }
+    }
+    
 </style>
