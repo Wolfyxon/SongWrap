b5fa695817750eba7c5e3f49f0278209466be52f
Wolfyxon <wolfyxon@gmail.com>
2025-12-12 13:07:33 +0100

    Init about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Page from "$lib/comp/Page.svelte";
+</script>
+
+<Page>
+    <h1>About SongWrap</h1>
+    <a href=".">Go back to home</a>
+
+    <p>
+        SongWrap is an online tool for viewing your song listening stats, inspired by Spotify Wrapped.
+    </p>
+    <p>
+        The cover arts for each song are retrieved from <a href="https://musicbrainz.org/">MusicBrainz</a>
+        and are not stored on this website.
+    </p>
+</Page>
