bda4db867fb9079afaf2878f5147f92eb4f65296
Wolfyxon <wolfyxon@gmail.com>
2025-12-29 12:32:53 +0100

    Added error display system

--- src/lib/comp/FileUpload.svelte
+++ src/lib/comp/FileUpload.svelte
@@ -1,22 +1,45 @@
 <script lang="ts">
+    import { error } from '@sveltejs/kit';
     import { onMount } from 'svelte'
 
-    export let label: string | null = null;
+    type OnDroppedCallbackResult = void | string | string[];
+    type OnDroppedCallback = (files: FileList) => OnDroppedCallbackResult | Promise<OnDroppedCallbackResult>;
 
-    export let onDropped: ((files: FileList) => any) = (files) => {
+    export let label: string | null = null;
+    let errorString = "";
+
+    export let onDropped: OnDroppedCallback = (files) => {
         console.warn("Files were dropped but 'onDropped' callback is not set");
         console.log(files);
+        
+        return "Wolfyxon forgot to implement this";
     };
     
     let fileHover = false;
 
     // TODO: Error display
 
-    function filesDropped(files: FileList | null | undefined) {
+    async function filesDropped(files: FileList | null | undefined) {
         if(!files || files.length == 0)
             return;
 
-        onDropped(files);
+        const errorRes: OnDroppedCallbackResult = await onDropped(files);
+        const errType = typeof(errorRes);
+
+        switch(errType) {
+            case "undefined":
+                errorString = "";
+                break;
+            case "string":
+                errorString = errorRes as string;
+                break;
+            case "object":
+                if(Array.isArray(errorRes)) {
+                    errorString = errorRes.join("\n");
+                }
+            
+                break;
+        }
     }
 
     function inputChange(e: Event) {
@@ -70,7 +93,12 @@
     }
 
     .file-upload-content h1 {
-        margin-top: 0;
+        margin: 0;
+    }
+
+    .file-upload-error {
+        color: red;
+        height: 2em;
     }
 
 </style>
@@ -81,6 +109,11 @@
             <h1>{label}</h1>
         {/if}
 
-        Drag and drop a file or use <input type="file" onchange={inputChange} aria-label="file picker" />
+        <div class="file-upload-error">
+            {errorString}
+        </div>
+        <div>
+            Drag and drop a file or use <input type="file" onchange={inputChange} aria-label="file picker" />
+        </div>
     </div>
 </div>
