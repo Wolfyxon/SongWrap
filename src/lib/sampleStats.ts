cbbc362e4dce1e773f3af16df979118dcbbbea1d
Wolfyxon <wolfyxon@gmail.com>
2025-12-29 11:47:52 +0100

    Added more songs to bump the numbers lol

--- src/lib/sampleStats.ts
+++ src/lib/sampleStats.ts
@@ -14,9 +14,24 @@ export const sampleStats: StatsData = {
             totalPlays: 35
         },
         {
+            title: "Armee der Tristen",
+            artist: "Rammstein",
+            totalPlays: 12
+        },
+        {
             title: "Behold the skeleton",
             artist: "KaroleusPL",
             totalPlays: 42
+        },
+        {
+            title: "Glitched Sword",
+            artist: "KaroleusPL",
+            totalPlays: 11
+        },
+        {
+            title: "Steampunk eye",
+            artist: "KaroleusPL",
+            totalPlays: 4
         },
         {
             title: "MEGALOVANIA",
@@ -32,6 +47,71 @@ export const sampleStats: StatsData = {
             title: "An Absence",
             artist: "Heaven Pierce Her",
             totalPlays: 30
+        },
+        {
+            title: "War Without Reason",
+            artist: "Heaven Pierce Her",
+            totalPlays: 13
+        },
+        {
+            title: "Castle Vein",
+            artist: "Heaven Pierce Her",
+            totalPlays: 3
+        },
+        {
+            title: "The Final Flash Of Existence",
+            artist: 'Jacek "Fragik" Rogal',
+            totalPlays: 5
+        },
+        {
+            title: "HORNSWOGGLE THE FLEET",
+            artist: "certified bot",
+            totalPlays: 5
+        },
+        {
+            title: "ULTRABIT",
+            artist: "certified bot",
+            totalPlays: 7
+        },
+        {
+            title: "BIG CHUNGUS",
+            artist: "Endigo",
+            totalPlays: 2
+        },
+        {
+            title: "Clair de Lune",
+            artist: "Debussy",
+            totalPlays: 3
+        },
+        {
+            title: "To the Bone",
+            artist: "JT Music",
+            totalPlays: 7
+        },
+        {
+            title: "I love the piano",
+            artist: "Kasso",
+            totalPlays: 4
+        },
+        {
+            title: "Bad Apple!!",
+            artist: "ZUN",
+            totalPlays: 3
+        },
+        {
+            title: "GLITCH IN YOUR HEART",
+            artist: "gabemtnz",
+            totalPlays: 7
+        },
+        {
+            title: "Labyrinth",
+            artist: "CG5",
+            totalPlays: 4
+        },
+        {
+            title: "Peace",
+            artist: "MEUTE",
+            totalPlays: 8
         }
     ]
 };
