3460b154dd72cb0882313d56eff030d0d742bdab
Wolfyxon <wolfyxon@gmail.com>
2025-12-30 11:15:21 +0100

    Added base64 logic

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -3,3 +3,16 @@ export function wait(ms: number): Promise<undefined> {
         setTimeout(resolve, ms);
     });
 }
+
+export function base64encode(text: string): string {
+    const bytes = new TextEncoder().encode(text);
+    const binStr = Array.from(bytes, (byte) => String.fromCodePoint(byte)).join("");
+
+    return btoa(binStr);
+}
+
+export function base64decode(base64: string): string {
+    const bytes = Uint8Array.from(atob(base64), (m) => m.codePointAt(0)!);
+    
+    return new TextDecoder().decode(bytes);
+}
