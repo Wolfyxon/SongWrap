eded371960ad8c3966ce6f79de918590bd67278a
Wolfyxon <wolfyxon@gmail.com>
2025-12-30 16:07:51 +0100

    Added valuesToIndices()

--- src/lib/util/array.ts
+++ src/lib/util/array.ts
@@ -13,3 +13,17 @@ export function shareValues(a: any[], b: any[]): boolean {
 export function randChoice<T>(array: T[]): T | undefined {
     return array[randi(0, array.length - 1)];
 }
+
+export function valuesToIndices(array: any[], values: any[]): number[] {
+    const res: number[] = [];
+
+    for(const val of values) {
+        const idx = array.indexOf(val);
+
+        if(idx != -1) {
+            res.push(idx);
+        }
+    }
+
+    return res;
+}
