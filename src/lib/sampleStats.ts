17c3d48b0b52f4eefcb0e3abe197ace19c53b112
Wolfyxon <wolfyxon@gmail.com>
2025-12-31 14:33:51 +0100

    Added timestamps to all sample songs

--- src/lib/sampleStats.ts
+++ src/lib/sampleStats.ts
@@ -14,6 +14,8 @@ export const sampleStats: StatsData = {
             title: "Giftig",
             artist: "Rammstein",
             totalPlays: 35,
+            firstPlay: 1765471779,
+            lastPlay: 1767104940
         },
         {
             title: "Armee der Tristen",
@@ -48,57 +50,79 @@ export const sampleStats: StatsData = {
         {
             title: "An Absence",
             artist: "Heaven Pierce Her",
-            totalPlays: 30
+            totalPlays: 30,
+            firstPlay: 1766077030,
+            lastPlay: 1766496418
         },
         {
             title: "War Without Reason",
             artist: "Heaven Pierce Her",
-            totalPlays: 13
+            totalPlays: 13,
+            firstPlay: 1766091522,
+            lastPlay: 1767184281
         },
         {
             title: "Castle Vein",
             artist: "Heaven Pierce Her",
-            totalPlays: 3
+            totalPlays: 3,
+            firstPlay: 1766175330,
+            lastPlay: 1766176330
         },
         {
             title: "The Final Flash Of Existence",
             artist: 'Jacek "Fragik" Rogal',
-            totalPlays: 5
+            totalPlays: 5,
+            firstPlay: 1765451897,
+            lastPlay: 1766407707
         },
         {
             title: "HORNSWOGGLE THE FLEET",
             artist: "certified bot",
-            totalPlays: 5
+            totalPlays: 5,
+            firstPlay: 1766352086,
+            lastPlay: 1767090034
         },
         {
             title: "ULTRABIT",
             artist: "certified bot",
-            totalPlays: 7
+            totalPlays: 7,
+            firstPlay: 1766351778,
+            lastPlay: 1767090286
         },
         {
             title: "BIG CHUNGUS",
             artist: "Endigo",
-            totalPlays: 2
+            totalPlays: 2,
+            firstPlay: 1766352086,
+            lastPlay: 1767090034
         },
         {
             title: "Clair de Lune",
             artist: "Debussy",
-            totalPlays: 3
+            totalPlays: 3,
+            firstPlay: 1765999602,
+            lastPlay: 1767095808
         },
         {
             title: "To the Bone",
             artist: "JT Music",
-            totalPlays: 7
+            totalPlays: 7,
+            firstPlay: 1766351203,
+            lastPlay: 1766371203
         },
         {
             title: "I love the piano",
             artist: "Kasso",
-            totalPlays: 4
+            totalPlays: 4,
+            firstPlay: 1766852784,
+            lastPlay: 1767174513
         },
         {
             title: "Bad Apple!!",
             artist: "ZUN",
-            totalPlays: 3
+            totalPlays: 3,
+            firstPlay: 1765569886,
+            lastPlay: 1767182058
         },
         {
             title: "GLITCH IN YOUR HEART",
@@ -108,12 +132,16 @@ export const sampleStats: StatsData = {
         {
             title: "Labyrinth",
             artist: "CG5",
-            totalPlays: 4
+            totalPlays: 4,
+            firstPlay: 1765456867,
+            lastPlay: 1767106788
         },
         {
             title: "Peace",
             artist: "MEUTE",
-            totalPlays: 8
+            totalPlays: 8,
+            firstPlay: 1765462848,
+            lastPlay: 1767007798
         }
     ]
 };
